770b408340ad2c2e98bc701c7fdb0ea9bd6930d4
abhinavgaikwad264-droid <abhinavgaikwad264@gmail.com>
2026-09-09 00:24:44 +0530

    Update ShizukuManager.kt

--- app/src/main/java/com/ultron/agent/ShizukuManager.kt
+++ app/src/main/java/com/ultron/agent/ShizukuManager.kt
@@ -10,7 +10,7 @@ import kotlinx.coroutines.CompletableDeferred
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
-import moe.shizuku.Shizuku
+import rikka.shizuku.Shizuku
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
